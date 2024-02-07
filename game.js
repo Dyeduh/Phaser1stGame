4c82af825ef5e775c719556a3935c0e37ffca6f7
Dyeduh <158043974+Dyeduh@users.noreply.github.com>
2024-02-07 14:32:07 +0200

    Додали зірки, фізику для них і можливість їх збирати

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 var player;
 var platforms;
+var stars;
+var cursors;
 
 var config = {
     type: Phaser.AUTO,
@@ -71,6 +73,21 @@ function create() {
     cursors = this.input.keyboard.createCursorKeys();
 
     this.physics.add.collider(player, platforms);
+
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 }
+    });
+    
+    stars.children.iterate(function (child) {
+    
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    
+    });
+
+    this.physics.add.collider(stars, platforms);
+    this.physics.add.overlap(player, stars, collectStar, null, this);
 }
 
 function update() {
@@ -93,4 +110,9 @@ function update() {
     if (cursors.up.isDown && player.body.touching.down) {
         player.setVelocityY(-330);
     }
+}
+
+function collectStar (player, star)
+{
+    star.disableBody(true, true);
 }
